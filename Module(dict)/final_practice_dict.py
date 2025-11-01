b1acc8a02b3fe6acc271f85bfb136d42ab4eab68
Candromid <mainaccnurbek@mail.ru>
2025-11-01 11:09:52 +0500

    added new program about struct_of_data and practice with output and change info in data

--- Module(dict)/final_practice_dict.py
+++ Module(dict)/final_practice_dict.py
@@ -27,6 +27,7 @@ for _ in range(count_of_songs):
 print("Общее время звучания песен: {:.2f}".format(total_min))
 """
 
+from pprint import pprint
 
 data = {
     "address": "0x544444444444",
@@ -52,7 +53,7 @@ data = {
             },
             "balance": 5000,
             "totalIn": 0,
-            "total_out": 0
+            "total_out": 2
         },
         {
             "sec_token_info": {
@@ -69,11 +70,38 @@ data = {
             },
             "balance": 500,
             "totalIn": 0,
-            "total_out": 0
+            "total_out": 1
         }
     ]
 }
-
+print("Структура данных: ")
+pprint(data) # красивый вывод исходной структуры данных
+#В ETH добавить ключ total_diff со значением 100.
 data["ETH"]["total_diff"] = 100
+print("новый ключ в словаре ETH с названием total_diff и значением 100: ", data["ETH"])
+print()
 
+
+#Внутри fst_token_info значение ключа name поменять с fdf на doge
+data["tokens"][0]["fst_token_info"]["name"] = "doge"
+print("Измененное значение ключа name в fst_token_info с fdf на doge", data["tokens"][0]["fst_token_info"]["name"])
+print()
+
+#Удалить total_out из словарей внутри списка tokens и присвоить сумму этих значений в total_out внутри ETH.
+
+for i_list in range(len(data["tokens"])):  # i_list индексы в листе tokens проходимся по всей длине списка
+
+    value = data["tokens"][i_list].pop("total_out")    # удаляем ключ total_out  и его значение присваиваем в переменную value
+
+    data["ETH"]["totalOut"] += value   # складываем значение value в ключ totalOut который во внешнем словаре
+
+print("Измененное значение ключа totalOut во внешнем словаре: ", data["ETH"]["totalOut"])  #Итого ключ во внутренних словарях удалены, а их значения засуммированы в ключ во внешнем словаре
+print()
+
+
+#Внутри sec_token_info изменить название ключа price на total_price.
+print("Словарь sec_token_info с ключом price", data["tokens"][1]["sec_token_info"])
+boofer = data["tokens"][1]["sec_token_info"].pop("price")
+data["tokens"][1]["sec_token_info"]["total_price"] = boofer
+print("Словарь sec_token_info с заменённым ключом price на total_price", data["tokens"][1]["sec_token_info"])
 
